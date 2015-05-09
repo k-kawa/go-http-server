0906c3a6d14caf9702a02073de8f2f1183423497
Kohei Kawasaki <mynameiskawasaq@gmail.com>
2015-05-09 10:35:26 +0900

    Add source and README

---
+++ README.md
@@ -0,0 +1,22 @@
+go-http-server
+===
+
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)][license]
+
+The easist way to start a web server in your laptop.
+
+## Usage
+You just execute `go-http-server start` in your console.
+
+```bash
+$ go-http-server start
+```
+
+You can specify the port number the server should listen to like this.
+
+```bash
+$ go-http-server start --port 8080
+```
+
+## Author
+[Kohei Kawasaki](https://github.com/k-kawa)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/codegangsta/cli"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "go-http-server"
+	app.Usage = "Start web server and public your current directory."
+
+	app.Version = "0.0.1"
+	app.Authors = append(app.Authors, cli.Author{Name: "Kohei Kawasaki", Email: "mynameiskawasaq@gmail.com"})
+
+	app.Commands = []cli.Command{
+		{
+			Name: "start",
+			Aliases: []string{"s"},
+			Usage: "Start the web server",
+			Action: start,
+			Flags: []cli.Flag {
+				cli.StringFlag {
+					Name: "port, p",
+					Value: "8000",
+					Usage: "Port number to listen to",
+					EnvVar: "PORT",
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func start(c *cli.Context) {
+		port := c.String("port")
+		log.Fatal(http.ListenAndServe(":"+port, http.FileServer(http.Dir("."))))	
+}
